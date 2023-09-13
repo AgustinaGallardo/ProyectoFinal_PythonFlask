44955e99c15cfffcfd825908655fa8ca11e775ca
114038-GALLARDO-AGUSTINA <114038@tecnicatura.frc.utn.edu.ar>
2023-09-13 18:38:27 -0300

    carreras_gestor_routes_app y templates

--- modules/common/gestor_carrera.py
+++ modules/common/gestor_carrera.py
@@ -1,10 +1,27 @@
 from modules.common.gestor_comun import ResponseMessage, validaciones
 from modules.models.entities import Facultad,Universidad,Campus,Programa,Carrera, db
+from config import registros_por_pagina
 
 class gestor_carrera(ResponseMessage):
 
 	def __init__(self):
 		super().__init__()
+
+
+	def obtener_pagina(self, pagina, **kwargs):
+		query = Carrera.query
+		if 'universidad' in kwargs:
+			query = query.filter(Carrera.universidad.ilike(f"%{kwargs['universidad']}%"))
+		if 'facultad' in kwargs:
+			query = query.filter(Carrera.facultad.ilike(f"%{kwargs['facultad']}%"))
+		if 'campus' in kwargs:
+			query = query.filter(Carrera.campus.ilike(f"%{kwargs['campus']}%"))
+		if 'programa' in kwargs:
+			query = query.filter(Carrera.programa.ilike(f"%{kwargs['programa']}%"))
+		
+		carreras, total_paginas = Carrera.obtener_paginado(query, pagina, registros_por_pagina)
+		return carreras, total_paginas
+	
 		
 	def consultar_carreras(self, **kwargs):
 
@@ -50,5 +67,4 @@ class gestor_carrera(ResponseMessage):
 				# Manejo de errores en caso de fallo en la consulta
 				db.session.rollback()
 				raise e
-		
 		
--- modules/routes_carreras.py
+++ modules/routes_carreras.py
@@ -1,11 +1,16 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, render_template,flash, request, redirect, url_for
+from flask_login import login_required
 from modules.common.gestor_carrera import gestor_carrera
+from flask import Blueprint
+from modules.auth import csrf
 
 carreras_bp = Blueprint('routes_carreras', __name__)
 
 @carreras_bp.route('/carreras', methods=['GET'])
-def obtener_carreras():
-    carreras = gestor_carrera().consultar_carreras()
-    carreras_data = [carrera.serialize() for carrera in carreras]
-    return jsonify({"Exito": True, "MensajePorFallo": None, "Resultado": carreras_data})
+@login_required
+def obtener_lista_paginada():
+    page = request.args.get('page', default=1, type=int)
+    
+    carreras, total_paginas = gestor_carrera().obtener_pagina(page)
 
+    return render_template('carreras/carreras.html',carreras=carreras, total_paginas=total_paginas, csrf=csrf)

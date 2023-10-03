2ea1c8d21844a8ac90ff2a5440f88777b4b89719
soleribone <lucisolrib@gmail.com>
2023-10-03 17:09:41 -0300

    Funciona el botón eliminar en editar carrera

--- modules/common/gestor_carrera.py
+++ modules/common/gestor_carrera.py
@@ -2,6 +2,8 @@ from modules.common.gestor_comun import ResponseMessage, validaciones
 from modules.models.entities import Facultad,Universidad,Campus,Programa,Carrera, db
 from config import registros_por_pagina
 
+
+
 class gestor_carrera(ResponseMessage):
 
 	def __init__(self):
@@ -109,6 +111,9 @@ class gestor_carrera(ResponseMessage):
 		return programas
 
 
+
+
+
 	def editar_carrera(self, carrera_id, **kwargs):
 			# Busca la carrera por su ID
 			carrera = Carrera.query.get(carrera_id)
@@ -120,8 +125,12 @@ class gestor_carrera(ResponseMessage):
 					Exito=False,
 					MensajePorFallo='La carrera no fue encontrada.'
 				)
+			
+			
+	
 
-			# Actualiza los campos de la carrera con los valores proporcionados
+			
+# Actualiza los campos de la carrera con los valores proporcionados
 			if 'facultad' in kwargs:
 				carrera.facultad = kwargs['facultad']
 			if 'universidad' in kwargs:
@@ -131,6 +140,7 @@ class gestor_carrera(ResponseMessage):
 			if 'programa' in kwargs:
 				carrera.programa = kwargs['programa']
 
+			
 			# Guarda los cambios en la base de datos
 			resultado_actualizar = carrera.guardar()
 
@@ -141,13 +151,18 @@ class gestor_carrera(ResponseMessage):
 				MensajePorFallo=resultado_actualizar["MensajePorFallo"]
 			)
 	
+			
+
+
+
+
 	def eliminar(self, id):
 		carrera = Carrera.query.get(id)
 		if carrera==None:
 			self.Exito = False
 			self.MensajePorFallo = "La carrera no existe"
 			return self.obtenerResultado()
-		resultado_borrar=carrera.borrar()
+		resultado_borrar=carrera.activar(False) 
 		self.Exito=resultado_borrar["Exito"]
 		self.MensajePorFallo=resultado_borrar["MensajePorFallo"]
 		return self.obtenerResultado()
